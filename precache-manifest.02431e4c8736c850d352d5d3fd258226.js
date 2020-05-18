8104c43ec8f21a4abc5cf95c566db0ac6700fa9a
Debbie <debbieellis7@gmail.com>
2020-05-18 20:17:45 +0800

    Updates

---
+++ precache-manifest.02431e4c8736c850d352d5d3fd258226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0990b6243f12cc6637de89ff11b920a3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2dbbd6c85fca2ddcb88f",
+    "url": "/monsters-rolodex/static/css/main.923cf71f.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dbbd6c85fca2ddcb88f",
+    "url": "/monsters-rolodex/static/js/main.095798aa.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
